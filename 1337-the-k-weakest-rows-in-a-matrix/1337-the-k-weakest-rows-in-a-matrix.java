fa3019ed731a8be53fe536f44ee980a98cf8465c
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-08-18 14:24:40 +0530

    Time: 2 ms (76.07%), Space: 45.1 MB (10.60%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int len = mat.length;
-        int[] arr = new int[len];
-        for(int i=0;i<len;i++)
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->a[1]!=b[1]?a[1]-b[1]:a[0]-b[0]);
+        
+        for(int i=0;i<mat.length;i++){
+            int count = 0;
             for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==1)
-                    arr[i]++;
-                else
+                if(mat[i][j]!=1)
                     break;
+                count++;
             }
-        int[] temp = Arrays.copyOfRange(arr,0,mat.length);
-        Arrays.sort(temp);
-        int[] ans = new int[k];
-        System.out.print(Arrays.toString(arr)+" "+Arrays.toString(temp));
-        for(int i=0;i<k;i++){
-            for(int j=0;j<temp.length;j++){
-                if(temp[i] == arr[j]){
-                    ans[i] = j;
-                    arr[j] = -1;
-                    break;
-                    
-                }
-            }
+            q.offer(new int[]{i,count});
         }
+        int[] ans = new int[k];
+        for(int i=0;i<k;i++)
+            ans[i] = q.poll()[0];
         return ans;
+        
     }
 }
