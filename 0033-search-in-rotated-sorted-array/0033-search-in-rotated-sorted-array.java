12dad9668958cdd8e6659c3118833125aff55a7e
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-08-23 23:05:31 +0530

    Time: 14 ms (5.01%), Space: 41.7 MB (8.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,24 @@
-class Solution {
+ class Solution {
     public int search(int[] nums, int target) {
-        if(target<nums[0]){
-            int low = 0, high = nums.length-1;
-            while(low<=high){
-                int mid = low + (high-low)/2;
-                if(nums[mid] == target)
-                    return mid;
-                else if((nums[mid]>target && nums[mid]>nums[nums.length-1]) || nums[mid]<target)
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            System.out.println(low+" "+high+" "+mid);
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid]<nums[nums.length-1]){
+                if(target>nums[mid] && target <= nums[nums.length-1])
                     low = mid+1;
                 else
                     high = mid-1;
             }
-            return -1;
-        }
-        else{
-           int low = 0, high = nums.length-1;
-            while(low<=high){
-                int mid = low + (high-low)/2;
-                if(nums[mid] == target)
-                    return mid;
-                else if(nums[mid]>target || (nums[mid]<target && nums[mid]<nums[0]))
+            else{
+                if(target>=nums[0] && target<nums[mid])
                     high = mid-1;
                 else
                     low = mid+1;
             }
-            return -1; 
         }
+        return -1;
     }
 }
