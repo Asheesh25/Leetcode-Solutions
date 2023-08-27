a33951e8ea0300746e03502458dcef2731555513
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-08-27 23:17:07 +0530

    Time: 0 ms (100%), Space: 41.3 MB (44.58%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,14 +2,18 @@ class Solution {
     public int findPeakElement(int[] nums) {
         if(nums.length == 1)
             return 0;
-        int[] arr = Arrays.copyOfRange(nums,0,nums.length);
-        Arrays.sort(arr);
-        for(int i=1;i<nums.length;i++)
-            if(nums[i-1]<nums[i] && nums[i] == arr[arr.length-1])
+        for(int i=0;i<nums.length;i++){
+            if(i == 0){
+                if(nums[i]>nums[i+1])
+                    return i;
+            }
+            else if(i == nums.length-1){
+                if(nums[i]>nums[i-1])
+                    return i;
+            }
+            else if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
                 return i;
-        for(int i=0;i<nums.length-1;i++)
-            if(nums[i]>nums[i+1] && nums[i] == arr[arr.length-1])
-                return i;
+        }
         return -1;
     }
 }
