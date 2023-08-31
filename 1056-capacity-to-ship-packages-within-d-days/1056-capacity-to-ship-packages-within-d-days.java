a22cbc59363c604097ec8b20ae52e956870f6176
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-08-31 11:47:02 +0530

    Time: 9 ms (97.5%), Space: 44.9 MB (77.62%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -6,19 +6,15 @@ class Solution {
                 max = wt;
             sum+=wt;
         }
-        int[] arr = new int[sum-max+1];
-        int index = 0;
-        for(int i=max;i<=sum;i++)
-            arr[index++] = i;
-        int  low = 0, high = arr.length-1;
+        int  low = max, high = sum;
         while(low<high){
             int mid = low + (high-low)/2;
-            if(fun(arr[mid],weights)>days)
+            if(fun(mid,weights)>days)
                 low = mid+1;
             else
                 high = mid;
         }
-        return arr[low];
+        return low;
     }
     int fun(int capacity, int[] arr){
         int day = 1, load = 0;
