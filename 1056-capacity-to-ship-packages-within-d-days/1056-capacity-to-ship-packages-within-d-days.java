4bb859357b3d73b852f5016e1bd2ac5682fbb3d7
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-08-31 11:42:45 +0530

    Time: 103 ms (5.37%), Space: 141.6 MB (6.35%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,47 +1,35 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int sum = 0,max = 0;
-        for(int num:weights){
-            sum+=num;
-            max = num>max?num:max;
+        int max = 0, sum = 0;
+        for(int wt:weights){
+            if(wt>max)
+                max = wt;
+            sum+=wt;
         }
         int[] arr = new int[sum-max+1];
         int index = 0;
         for(int i=max;i<=sum;i++)
             arr[index++] = i;
-        for(int i=1;i<weights.length;i++)
-            weights[i]+=weights[i-1];
-        int low = 0, high = arr.length-1;
+        int  low = 0, high = arr.length-1;
         while(low<high){
             int mid = low + (high-low)/2;
-            if(count(weights,arr[mid])>days)
+            if(fun(arr[mid],weights)>days)
                 low = mid+1;
             else
                 high = mid;
         }
         return arr[low];
     }
-    int count(int[] arr, int target){
-        int index = 0, ans = 0,init = 0;
-        while(index<arr.length){
-            if(arr[index]-init<target){
-                if(index == arr.length-1){
-                    ans++;
-                    break;
-                }
-                index++;
-            }
-            else if(arr[index]-init>target){
-                ans+=1;
-                index--;
-                init = arr[index];
-            }
+    int fun(int capacity, int[] arr){
+        int day = 1, load = 0;
+        for(int i=0;i<arr.length;i++){
+            if(load+arr[i]<=capacity)
+                load+=arr[i];
             else{
-                ans+=1;
-                init = arr[index];
-                index++;
+                day+=1;
+                load = arr[i];
             }
         }
-        return ans;
+        return day;
     }
 }
