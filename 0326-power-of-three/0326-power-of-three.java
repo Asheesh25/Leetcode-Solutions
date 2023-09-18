0a39bb2a47c67810f7b08adeec7012a9df29a178
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-09-18 20:22:25 +0530

    Time: 12 ms (25.46%), Space: 42.7 MB (58.69%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n == 2147483647)
-            return false;
         int i = 0;
         while(true){
-            int a = (int)Math.pow(3,i++);
+            long a = (long)Math.pow(3,i++);
             if(a == n)
                 return true;
             if(a>n)
