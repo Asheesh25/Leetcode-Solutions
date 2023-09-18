f867e7bed4d7bd5a075c33eb6c95c78bc09cd6f9
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-09-18 19:57:38 +0530

    Time: 1 ms (78.88%), Space: 40 MB (12.4%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {       
-        if(n<=0)
-            return false;
-        if((n&(n-1)) == 0 && Integer.toBinaryString(n).length()%2==1)
-            return true;
-        return false;     
+        return n<=0 ? false : ((n&(n-1)) == 0 && Integer.toBinaryString(n).length()%2 == 1)? true:false;
     }
 }
