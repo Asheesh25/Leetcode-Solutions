4fe138428e73e480718adc8b1cce6de02349a478
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-09-29 10:57:55 +0530

    Time: 0 ms (100%), Space: 39.9 MB (99.48%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+        // double ans = 1;
+        // int b = 1;
+        // if(n==0)
+        //     return 1;
+        // else if(n>0){
+        //     for(int i=0;i<n;i++)
+        //         ans*=x;
+        // }
+        // else{
+        //     for(int i=0;i<n;i++){
+        //         b*=x;
+        //         ans = 1/b;
+        //         System.out.print(ans+" ");
+        //     }
+        // }
+        // return ans;
+    }
+}
