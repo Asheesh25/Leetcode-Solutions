801c16c9b35033480b4dde7654ae85a7862e8f72
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-10-10 23:49:46 +0530

    Time: 0 ms (100%), Space: 41 MB (18.82%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        int [][] ans = new int[n][m];
-        for(int i = 0;i<n;i++){
-            if(obstacleGrid[i][0] == 0) ans[i][0] = 1;
-            else break;
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+        if(obstacleGrid[0][0] == 1)
+            return 0;
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0] == 0)
+                dp[i][0] = 1;
+            else
+                break;
         }
-        for(int j = 0;j<m;j++){
-            if(obstacleGrid[0][j] == 0) ans[0][j] = 1;
-            else break;
+        for(int i=0;i<n;i++){
+            if(obstacleGrid[0][i] == 0)
+                dp[0][i] = 1;
+            else
+                break;
         }
 
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<m;j++){
-                if(obstacleGrid[i][j]==0) ans[i][j] = ans[i-1][j]+ans[i][j-1];
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j] == 0)
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
             }
         }
-        return ans[n-1][m-1];
+        return dp[m-1][n-1];
     }
 }
