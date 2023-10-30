7cdd545e55031f59e6525047e7aa3dc856ccb77d
Asheesh Upadhyay <86250927+Asheesh25@users.noreply.github.com>
2023-10-30 18:34:50 +0530

    Time: 2 ms (73.68%), Space: 41.5 MB (58.77%) - LeetHub

--- 2892-check-if-array-is-good/2892-check-if-array-is-good.java
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean isGood(int[] nums) {
         Arrays.sort(nums);
         int num = nums.length;
-        if(nums[num-1]!=num-1)
-            return false;
-        if(nums[num-1] !=num-1 || nums[num-2]!=num-1)
+        if(nums[num-1]!=num-1 || nums[num-1] !=num-1 || nums[num-2]!=num-1)
             return false;
         for(int i=0;i<num-2;i++)
             if(nums[i]!=i+1)
